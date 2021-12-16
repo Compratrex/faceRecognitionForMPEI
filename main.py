11fa72d743047a3dc47d72cc4ed0a3775e4856f2
George <76513693+Compratrex@users.noreply.github.com>
2021-12-16 17:55:25 +0300

    Добавили новые функции

--- main.py
+++ main.py
@@ -4,25 +4,22 @@ from scipy.spatial import distance
 
 
 class CheckPhotos():
-    def __init__(self, photo1, photo2, rec_model, shape_perdictor):
-        self.photo1 = photo1
-        self.photo2 = photo2
+    def __init__(self, rec_model, shape_perdictor):
         self.rec_model = rec_model
         self.shape_perdictor = shape_perdictor
 
-    def check_two_photos(self):
+    def check_two_photos(self, photo1, photo2):
         sp = dlib.shape_predictor(self.shape_perdictor)
         facerec = dlib.face_recognition_model_v1(self.rec_model)
         detector = dlib.get_frontal_face_detector()
 
-        img = io.imread(self.photo1)
+        img = io.imread(photo1)
 
         win1 = dlib.image_window()
         win1.clear_overlay()
         win1.set_image(img)
 
         dets = detector(img, 1)
-
         for k, d in enumerate(dets):
             # print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
             #     k, d.left(), d.top(), d.right(), d.bottom()))
@@ -33,7 +30,7 @@ class CheckPhotos():
 
         face_descriptor1 = facerec.compute_face_descriptor(img, shape)
 
-        img = io.imread(self.photo2)
+        img = io.imread(photo2)
         win2 = dlib.image_window()
         win2.clear_overlay()
         win2.set_image(img)
@@ -47,15 +44,49 @@ class CheckPhotos():
             win2.add_overlay(shape)
 
         face_descriptor2 = facerec.compute_face_descriptor(img, shape)
-
         a = distance.euclidean(face_descriptor1, face_descriptor2)
-
+        print(a)
         if a > 0.6:
             return False
         return True
+    def Descriptor(self, photo1):
+        sp = dlib.shape_predictor(self.shape_perdictor)
+        facerec = dlib.face_recognition_model_v1(self.rec_model)
+        detector = dlib.get_frontal_face_detector()
 
+        img = io.imread(photo1)
+        dets = detector(img, 1)
+        for k, d in enumerate(dets):
+            shape = sp(img, d)
+        face_descriptor1 = facerec.compute_face_descriptor(img, shape)
+        return face_descriptor1
+    def checkTwoDesc(self, desc1, desc2):
+        dist = distance.euclidean(desc1, desc2)
+        if dist > 0.6:
+            return False
+        return True
+    def checkPhotoAndDesc(self, photo, descriptor):
+        sp = dlib.shape_predictor(self.shape_perdictor)
+        facerec = dlib.face_recognition_model_v1(self.rec_model)
+        detector = dlib.get_frontal_face_detector()
 
-# shape = 'shape_predictor_68_face_landmarks.dat'
-# rec_mdl = 'dlib_face_recognition_resnet_model_v1.dat'
-# check = CheckPhotos('elon.jpg', 'putin.jpg', rec_mdl, shape)
-# print(check.check_two_photos())
+        img = io.imread(photo)
+
+        win1 = dlib.image_window()
+        win1.clear_overlay()
+        win1.set_image(img)
+
+        dets = detector(img, 1)
+
+        for k, d in enumerate(dets):
+            shape = sp(img, d)
+            win1.clear_overlay()
+            win1.add_overlay(d)
+            win1.add_overlay(shape)
+
+        face_descriptor = facerec.compute_face_descriptor(img, shape)
+        dist = distance.euclidean(descriptor, face_descriptor)
+        if dist > 0.6:
+            return False
+        return True
+
